91896d2cd8669f02ec505817d033a6fd5819bb87
Farhan <farhan@dynexcel.com>
2021-08-21 23:24:36 +0000

    update workflow

--- de_project_task_workflow/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_project_task_workflow/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,27 +1,26 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Task Workflow",
-
     'summary': """
         Task Workflow through stages
         """,
-
     'description': """
         Project Task Workflow
     """,
-
     'author': "Dynexcel",
     'website': "http://www.dynexcel.com",
-
     'category': 'Project',
     'version': '14.0.0.3',
-
-    # any module necessary for this one to work correctly
+    "price": 75,
+    "currency": "USD",
     'depends': ['project'],
-
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/mail_data.xml',
         'views/project_views.xml',
     ],
+    "auto_install": False,
+    "installable": True,
+    "live_test_url":'https://www.youtube.com/watch?v=2A6UAiSnpW0',
+    "images":['static/description/banner.jpg'],
 }
--- de_project_task_workflow/models/.ipynb_checkpoints/project-checkpoint.py
+++ de_project_task_workflow/models/.ipynb_checkpoints/project-checkpoint.py
@@ -13,8 +13,7 @@ from odoo.osv.expression import OR
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
     
-    stage_code = fields.Char(string='Code', size=3)
-    
+    user_id = fields.Many2one('res.users', string='Assigned to', default=lambda self: self.env.uid, )
     stage_category = fields.Selection([
         ('draft', 'Draft'),
         ('progress', 'In Progress'),
@@ -25,10 +24,10 @@ class ProjectTaskType(models.Model):
     
     @api.constrains('sequence')
     def _check_sequence(self):
-        ex_task_type_id = env['project.task.type']
+        ex_task_type_id = self.env['project.task.type']
         for record in self:
-            ex_task_type_id = env['project.task.type'].search([('id','!=',record.id),('sequence','=',record.name),('active','=',True)],limit=1)
-            if record.ex_task_type_id:
+            ex_task_type_id = self.env['project.task.type'].search([('id','!=',record.id),('sequence','=',record.sequence),('active','=',True)],limit=1)
+            if ex_task_type_id.sequence:
                 raise ValidationError(_('Sequence already found.'))
     
 class ProjectTask(models.Model):
@@ -38,7 +37,6 @@ class ProjectTask(models.Model):
     
     next_stage_id = fields.Many2one('project.task.type', string='Next Stage', compute='_compute_task_stage')
     prv_stage_id = fields.Many2one('project.task.type', string='Previous Stage', compute='_compute_task_stage')
-    stage_code = fields.Char(related='stage_id.stage_code')
     stage_category = fields.Selection(related='stage_id.stage_category')
     date_submit = fields.Datetime('Submission Date', readonly=False)
     date_approved = fields.Datetime('Approved Date', readonly=False)
@@ -46,6 +44,7 @@ class ProjectTask(models.Model):
     
     def write(self, vals):
         stage_id = self.env['project.task.type']
+        user = False
         result = super(ProjectTask,self).write(vals)
          # stage change: update date_last_stage_update
         if 'stage_id' in vals:
@@ -57,26 +56,35 @@ class ProjectTask(models.Model):
                         raise UserError(_("You are not authorize to approve '%s'.", stage_id.name))
                 
                 if task.stage_id.stage_category == 'draft':
-                    if task.order_stage_ids:
+                    if task.task_stage_ids:
                         task.task_stage_ids.unlink()
-                    self._compute_task_stages()
+                    self._compute_task_all_stages()
+                    
+                if task.stage_id.user_id:
+                    user = task.stage_id.user_id.id
+                else:
+                    user = task.user_id.id
+                task.update({
+                    'user_id': user,
+                })
+                
         return result
     
-    def _compute_task_stages(self):
+    def _compute_task_all_stages(self):
         stages_list = []
         next_stage = prv_stage = False
         for task in self:
             stage_ids = self.env['project.task.type'].search([('project_ids', '=', task.project_id.id)])
             for stage in stage_ids:
                 stages_list.append({
-                    'project_id': task.id,
+                    'task_id': task.id,
                     'stage_id': stage.id, 
                     'sequence': stage.sequence,
                 })            
             task.task_stage_ids.sudo().create(stages_list)
             #===================================
             #++++++++Assign Next Stage++++++++++++++
-            stages = self.env['project.task.stage'].search([('project_id','=', task.id)], order="sequence desc")
+            stages = self.env['project.task.stage'].search([('task_id','=', task.id)], order="sequence desc")
             for stage in stages:
                 stage.sudo().update({
                     'next_stage_id': next_stage,
@@ -84,7 +92,7 @@ class ProjectTask(models.Model):
                 next_stage = stage.stage_id.id
             #++++++++++++++++++++++++++++++++++++++++
             #+++++++++++Assign Previous Stage++++++++++
-            stages = self.env['project.task.stage'].search([('project_id','=', task.id)], order="sequence asc")
+            stages = self.env['project.task.stage'].search([('task_id','=', task.id)], order="sequence asc")
             for stage in stages:
                 stage.sudo().update({
                     'prv_stage_id': prv_stage,
@@ -107,46 +115,97 @@ class ProjectTask(models.Model):
             task.prv_stage_id = prv_stage
             
     def action_submit(self):
+        user = False
         for task in self.sudo():
             group_id = task.stage_id.group_id
             if group_id:
                 if not (group_id & self.env.user.groups_id):
                     raise UserError(_("You are not authorize to submit task."))
+            
+            if not task.task_stage_ids:
+                self._compute_task_all_stages()
+                
+            if task.next_stage_id.user_id:
+                user = task.next_stage_id.user_id.id
+            else:
+                user = task.user_id.id
+        self.activity_update()
         self.update({
             'stage_id' : self.next_stage_id.id,
             'date_submit' : fields.Datetime.now(),
+            'user_id': user,
         })
         
     def action_confirm(self):
+        user = False
         for task in self.sudo():
             group_id = task.stage_id.group_id
             if group_id:
                 if not (group_id & self.env.user.groups_id):
                     raise UserError(_("You are not authorize to approve '%s'.", task.stage_id.name))
         
-        if not self.next_stage_id:
-            raise UserError(_("No stage to move forward."))
-            
+            if not task.next_stage_id:
+                raise UserError(_("No stage to move forward."))
+                
+            if task.next_stage_id.user_id:
+                user = task.next_stage_id.user_id.id
+            else:
+                user = task.user_id.id
+        
+        notification = {
+            'type': 'ir.actions.client',
+            'tag': 'display_notification',
+            'params': {
+                'title': _('There are no task to approve.'),
+                'type': 'warning',
+                'sticky': False,  #True/False will display for few seconds if false
+            },
+        }
         self.update({
             'date_approved' : fields.Datetime.now(),
             'stage_id' : self.next_stage_id.id,
+            'user_id': user,
         })
+        notification['params'].update({
+            'title': _('The task was successfully approved.'),
+            'type': 'success',
+            'next': {'type': 'ir.actions.act_window_close'},
+        })
+            
+        self.activity_update()
+        return notification
         
     def action_refuse(self):
+        user = False
         for task in self.sudo():
             group_id = task.stage_id.group_id
             if group_id:
                 if not (group_id & self.env.user.groups_id):
                     raise UserError(_("You are not authorize to approve '%s'.", task.stage_id.name))
         
-        if not self.prv_stage_id:
-            raise UserError(_("No stage to move backward."))
-            
+            if not task.prv_stage_id:
+                raise UserError(_("No stage to move backward."))
+                
+            if task.prv_stage_id.user_id:
+                user = task.prv_stage_id.user_id.id
+            else:
+                user = task.user_id.id
         self.update({
             'date_refused' : fields.Datetime.now(),
             'stage_id' : self.prv_stage_id.id,
+            'user_id': user,
         })
 
+    def _get_responsible_for_approval(self):
+        if self.user_id:
+            return self.user_id
+        return self.env['res.users']
+    
+    def activity_update(self):
+        for task in self.filtered(lambda hol: hol.stage_category == 'draft'):
+            self.activity_schedule('de_project_task_workflow.mail_act_task_approval', user_id=task.sudo()._get_responsible_for_approval().id or self.env.user.id)
+        self.filtered(lambda hol: hol.stage_category == 'progress').activity_feedback(['de_project_task_workflow.mail_act_task_approval'])
+        self.filtered(lambda hol: hol.stage_category in ('draft')).activity_unlink(['de_project_task_workflow.mail_act_task_approval'])
 
 class ProjectTaskStage(models.Model):
     _name = 'project.task.stage'
