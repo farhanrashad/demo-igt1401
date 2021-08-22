2ebc4eadd85f70a68baeecc84deffb112c36eb39
farhan <farhan@dynexcel.com>
2021-08-22 15:18:52 +0000

    update msa

--- de_msa/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_msa/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -15,7 +15,7 @@
     'author': "Dynexcel",
     'website': "https://www.dynexcel.com",
     'category': 'Sale',
-    'version': '14.0.1.2',
+    'version': '14.0.1.3',
 
     'depends': ['base','product','de_project_planning','account','contacts','account_asset'],
 
