f7e3b3dc2f098c03d310a9b11f05c4d1357bb6c8
AhmadRaza2446 <ahmadraza2446@gmail.com>
2021-08-24 08:19:41 +0000

    Update PO Deviation Report

--- de_po_deviation_report/models/.ipynb_checkpoints/po_deviation_report_xlsx-checkpoint.py
+++ de_po_deviation_report/models/.ipynb_checkpoints/po_deviation_report_xlsx-checkpoint.py
@@ -10,6 +10,7 @@ class GenerateXLSXReport(models.Model):
 
     def generate_xlsx_report(self, workbook, data, lines,model="ir.actions.report",output_format="xlsx",report_name="de_po_deviation_report.po_deviation_report_xlsx"):
         
+        
         format1 = workbook.add_format({'font_size': '12', 'align': 'vcenter', 'bold': True})
         sheet = workbook.add_worksheet('PO Deviation Report')
         sheet.write(3, 0, 'Order Reference', format1)
